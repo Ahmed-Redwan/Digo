47cf2a17ce60d70903bbf0a4d8d68b9ddfc0cc9a
Ameer <AmeerMuammer@gmail.com>
2022-10-31 17:18:25 +0200

    Initial commit

---
+++ app/src/main/java/android/training2022/digo/MainActivity.java
@@ -0,0 +1,16 @@
+package android.training2022.digo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
